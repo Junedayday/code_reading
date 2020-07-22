52c4bdb26c72654b13e721ba86b1bc2bf8aba026
junedayday <junedayday@gmail.com>
2020-07-22 20:24:00 +0800

    add ctx part

--- basic/basic.md
+++ basic/basic.md
@@ -7,3 +7,9 @@
 1. 理解WaitGroup的实现 - 核心是`CAS`的使用
 2. Add与Done应该放在哪？ - `Add放在Goroutine外，Done放在Goroutine中`，逻辑复杂时建议用defer保证调用
 3. WaitGroup适合什么样的场景？ - `并发的Goroutine执行的逻辑相同时`，否则代码并不简洁，可以采用其它方式
+
+### [Context](goroutine/ctx.go)
+
+1. Context上下文 - 结合Linux操作系统的`CPU上下文切换/子进程与父进程`进行理解
+2. 如何优雅地使用context - 与`select`配合使用，管理协程的生命周期
+3. Context的底层实现是什么？ - `mutex`与`channel`的结合，前者用于初始部分参数，后者用于通信
--- basic/goroutine/wg.go
+++ basic/goroutine/wg.go
@@ -5,12 +5,14 @@ import (
 	"sync"
 )
 
-func wg(){
+func wg() {
 	var wg sync.WaitGroup
-	for i:=0;i<10;i++{
+	for i := 0; i < 10; i++ {
 		wg.Add(1)
-		go func(i int){
-			fmt.Println(i)
+		go func(i int) {
+			if i < 5 {
+				fmt.Println(i)
+			}
 			defer wg.Done()
 		}(i)
 	}
@@ -19,24 +21,24 @@ func wg(){
 }
 
 // Tip: waitGroup 不要进行copy
-func errWg1(){
+func errWg1() {
 	var wg sync.WaitGroup
-	for i:=0;i<10;i++{
+	for i := 0; i < 10; i++ {
 		wg.Add(1)
-		go func(i int,wg sync.WaitGroup){
+		go func(i int, wg sync.WaitGroup) {
 			fmt.Println(i)
 			defer wg.Done()
-		}(i,wg)
+		}(i, wg)
 	}
 	wg.Wait()
 	fmt.Println("finished")
 }
 
 // Tip: waitGroup 的 Add 要在goroutine前执行
-func errWg2(){
+func errWg2() {
 	var wg sync.WaitGroup
-	for i:=0;i<10;i++{
-		go func(i int){
+	for i := 0; i < 10; i++ {
+		go func(i int) {
 			wg.Add(1)
 			fmt.Println(i)
 			defer wg.Done()
@@ -47,11 +49,11 @@ func errWg2(){
 }
 
 // Tip: waitGroup 的 Add 很大会有影响吗？
-func errWg3(){
+func errWg3() {
 	var wg sync.WaitGroup
-	for i:=0;i<10;i++{
+	for i := 0; i < 10; i++ {
 		wg.Add(100)
-		go func(i int){
+		go func(i int) {
 			fmt.Println(i)
 			defer wg.Done()
 			wg.Add(-100)
